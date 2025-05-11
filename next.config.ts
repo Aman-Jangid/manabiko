c4480893093c793bb28c21bf88d72521a8012af7
Aman-Jangid <aman2003jangid@gmail.com>
2025-05-12 05:01:36 +0530

    Remove gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ["covers.openlibrary.org"],
     unoptimized: true,
-  },
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/manabiko" : "",
-  trailingSlash: true,
-  // Configure static export
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      "/auth/signin": { page: "/auth/signin" },
-      "/auth/signup": { page: "/auth/signup" },
-      "/profile": { page: "/profile" },
-      "/uploadArea": { page: "/uploadArea" },
-      "/404": { page: "/404" },
-    };
   },
 };
 
