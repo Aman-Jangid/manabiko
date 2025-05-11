0ddf79091de699c05f1f3b6768ca3c818d0ff16b
Aman-Jangid <aman2003jangid@gmail.com>
2025-05-12 01:43:48 +0530

    Update gh-pages workflow

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,17 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === "production" ? "/manabiko" : "",
   trailingSlash: true,
+  // Configure static export
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/auth/signin": { page: "/auth/signin" },
+      "/auth/signup": { page: "/auth/signup" },
+      "/profile": { page: "/profile" },
+      "/uploadArea": { page: "/uploadArea" },
+      "/404": { page: "/404" },
+    };
+  },
 };
 
 export default nextConfig;
